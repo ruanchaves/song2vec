3ee858a3219cb5eeeaef1c4c5681799609122bf6
Ruan Chaves <ruanchaves93@gmail.com>
2018-08-05 09:07:04 -0300

    fixed install.sh bug

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 wget http://labrosa.ee.columbia.edu/millionsong/sites/default/files/challenge/train_triplets.txt.zip
 unzip train_triplets.txt.zip
 wget https://labrosa.ee.columbia.edu/millionsong/sites/default/files/AdditionalFiles/unique_tracks.txt
-python3.5 train.py
-python3.5 MSDtools.py
+python3.5 -m song2vec.train
+python3.5 -m song2vec.MSDtools
